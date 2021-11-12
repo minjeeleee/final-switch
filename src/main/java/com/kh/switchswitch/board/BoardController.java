21c26f884d14f541b0bf0820282df9f7e3e6f7d5
song <sgh83037@naver.com>
2021-11-12 18:10:49 +0900

    dev board

--- src/main/java/com/kh/switchswitch/board/BoardController.java
+++ src/main/java/com/kh/switchswitch/board/BoardController.java
@@ -1,8 +1,11 @@
 package com.kh.switchswitch.board;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,4 +17,10 @@ public class BoardController {
 	
 	@GetMapping("board-form")
 	public void boardForm() {}
+
+
+	@GetMapping("board-detail")
+	public void boardDetail() {}
+
 }
+
