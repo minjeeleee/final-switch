043624b4319eb5f8ba9162ed5df0ff25d81305e9
khj <kim37171@cuk.edu>
2021-11-16 19:37:22 +0900

    [김현진] MemberDTO

---
+++ src/main/java/com/kh/switchswitch/member/model/dto/Member.java
@@ -0,0 +1,22 @@
+package com.kh.switchswitch.member.model.dto;
+
+import java.time.LocalDate;
+
+import lombok.Data;
+
+@Data
+public class Member {
+	
+	private int memberIdx;
+	private String code;
+	private LocalDate memberRegDate;
+	private String memberPass;
+	private String memberEmail;
+	private int memberDelYn;
+	private String memberNick;
+	private String memberAddress;
+	private LocalDate memberDelDate;
+	private int memberScore;
+	private String memberName;
+	
+}
