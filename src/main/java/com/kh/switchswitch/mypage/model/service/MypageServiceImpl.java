0104544dadd7ced495aecfd1cd93166fb5528ce6
minjeeleee <fly12035@gmail.com>
2021-11-16 21:04:58 +0900

    test

--- src/main/java/com/kh/switchswitch/mypage/model/service/MypageServiceImpl.java
+++ src/main/java/com/kh/switchswitch/mypage/model/service/MypageServiceImpl.java
@@ -1,7 +1,10 @@
 package com.kh.switchswitch.mypage.model.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.kh.switchswitch.member.model.dto.Member;
+import com.kh.switchswitch.member.model.repository.MemberRepository;
 import com.kh.switchswitch.mypage.model.repository.MypageRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -11,10 +14,19 @@ import lombok.RequiredArgsConstructor;
 public class MypageServiceImpl implements MypageService{
 
 	private final MypageRepository mypageRepository;
+	private final MemberRepository memberRepository;
+	private final PasswordEncoder passwordEncoder;
 
 	@Override
 	public void leaveId(String email) {
-		// TODO Auto-generated method stub
-		
+		mypageRepository.leaveId(email);
+	}
+	
+	public int checkPwForLeave(Member member, String password) {
+		if(passwordEncoder.matches(member.getMemberPass(), password)) {
+			leaveId(member.getMemberEmail());
+			return 1;
+		}
+		return 0;
 	}
 }
