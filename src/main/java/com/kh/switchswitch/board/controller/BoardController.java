866f3586bf7356235faa56bb8cded02458a11a7b
song <sgh83037@naver.com>
2021-11-17 22:11:00 +0900

    community board - upload/datail/list 진행중

--- src/main/java/com/kh/switchswitch/board/controller/BoardController.java
+++ src/main/java/com/kh/switchswitch/board/controller/BoardController.java
@@ -1,18 +1,23 @@
 package com.kh.switchswitch.board.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.switchswitch.board.model.dto.Board;
+
 import com.kh.switchswitch.board.model.service.BoardService;
+import com.kh.switchswitch.member.model.dto.Member;
 
 import lombok.RequiredArgsConstructor;
 
@@ -27,13 +32,35 @@ public class BoardController {
 	
 	@GetMapping("board-form")
 	public void boardForm() {}
-
-	@PostMapping("upload")
-	public String uploadBoard() {
-		return null;
+	
+	
+	//11/17
+	//list받아오기
+	//paging처리
+	@GetMapping("board-list")
+	public void boardList(Model model, String bdIdx) {
+		//model.addAttribute("board", boardService.findBoardByIdx(bdIdx));
 	}
-
-
+	
+	@GetMapping("board-detail")
+	public void boardDetail(String bdIdx, Model model) {
+		Map<String,Object> commandMap = boardService.selectBoardByIdx(bdIdx);
+		model.addAttribute("datas", commandMap);
+	}
+	
+	@GetMapping("board-modify")
+	public void boardModify() {}
+	
+	
+	//11/17 500오류   수정필요
+	@PostMapping("upload") 
+	//NullPointerException
+	//: Cannot invoke "com.kh.switchswitch.member.model.dto.Member.getMemberEmail()" because "member" is null
+	public String uploadBoard(Board board,@SessionAttribute("authentication") Member member) {//required=false, name=
+		board.setUserId(member.getMemberNick());
+		boardService.insertBoard(board);
+		return "redirect:/board/board-detail";
+	}
 
 
 }
--- src/main/java/com/kh/switchswitch/board/model/service/BoardService.java
+++ src/main/java/com/kh/switchswitch/board/model/service/BoardService.java
@@ -1,13 +1,14 @@
 package com.kh.switchswitch.board.model.service;
 
-import java.util.List;
 import java.util.Map;
-
-import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.switchswitch.board.model.dto.Board;
 
 public interface BoardService {
-	void insertBoard(List<MultipartFile> mfs, Board board);
+	
+	void insertBoard(Board board);
+	
 	Map<String, Object> selectBoardByIdx(String bdIdx);
+
+
 }
