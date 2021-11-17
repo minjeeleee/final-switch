eb3b1969135ba1377c71c6c2e4e4cdaca8ba6627
minjeeleee <fly12035@gmail.com>
2021-11-17 21:21:20 +0900

    [이민지] 회원정보 js 수정

--- src/main/webapp/resources/js/member/modifyForm.js
+++ src/main/webapp/resources/js/member/modifyForm.js
@@ -7,7 +7,7 @@
 
 	  document.querySelector('#btnPwCheck').addEventListener('click', function(){
 		  
-		   let password = document.querySelector('#password').value;
+		   let password = document.querySelector('#memberPass').value;
 		   
 		   if(!password){
 			   document.querySelector('#passwordCheck').innerHTML = '비밀번호를 입력하지 않았습니다';
@@ -37,7 +37,7 @@
 
 	document.querySelector('#btnNickCheck').addEventListener('click', function(){
 		  
-		   let nickName = document.querySelector('#nickName').value;
+		   let nickName = document.querySelector('#memberNick').value;
 		   let nickReg = /^[\w\Wㄱ-ㅎㅏ-ㅣ가-힣]{2,10}$/;
 
 		   if(!nickName){
@@ -77,20 +77,32 @@
 	   });
 	   
 	   document.querySelector('#frm_modify').addEventListener('submit',e=>{
-		   let password = document.querySelector('#password').value;
-		   let nickName = document.querySelector('#nickName').value;
+		   let password = document.querySelector('#memberPass').value;
+		   let nickName = document.querySelector('#memberNick').value;
 			console.dir(password);
 		   
 			if(cofirmNick != nickName){
 			   alert('닉네임 중복 검사를 하지 않았습니다');
-			   document.querySelector('#nickName').focus();
+			   document.querySelector('#memberNick').focus();
 			   e.preventDefault();
 		   }
 
 		   if(cofirmPw != password){
 			   alert('비밀번호 일치 검사를 하지 않습니다');
-			   document.querySelector('#password').focus();
+			   document.querySelector('#memberPass').focus();
 			   e.preventDefault();
 		   }
+		   
+		   let file = e[0].name.split('.');
+			let filetype = file[file.length-1].toLowerCase();
+			if(filetype == 'jpg' || filetype == 'gif' || filetype == 'jpeg' || filetype=='png'){
+				
+			} else{
+				drawMsg('<i class="fas fa-times-circle"></i><br>JPG/GIF/PNG 파일만 업로드 가능합니다.');
+				let files = document.getElementsByName('teamFile');
+				console.dir(files);
+				files[0].value='';
+				return false;
+			}
 	   })
   })();
