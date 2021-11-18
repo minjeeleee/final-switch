e1436c3cfdc8837896d74a426c66c42793b5fae0
khj <kim37171@cuk.edu>
2021-11-18 22:32:46 +0900

    [공동] 공동 카드 생성 기능 완료

---
+++ src/main/java/com/kh/switchswitch/card/controller/CardController.java
@@ -0,0 +1,37 @@
+package com.kh.switchswitch.card.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.kh.switchswitch.card.model.dto.Card;
+import com.kh.switchswitch.card.model.service.CardService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("card")
+@RequiredArgsConstructor
+public class CardController {
+	
+	private final CardService cardService;
+
+	@GetMapping("card-form")
+	public void cardForm() {}
+	
+	@PostMapping("card-form")
+	public String createCard(@RequestParam(required = false) List<MultipartFile> imgList
+			//,@AuthenticationPrincipal MemberAccount member
+			, Card card
+			) {
+		
+		card.setMemberIdx(1);
+		cardService.insertCard(imgList, card);
+		return "redirect:/";
+	}
+}
--- src/test/java/com/kh/switchswitch/card/CardControllerTest.java
+++ src/test/java/com/kh/switchswitch/card/CardControllerTest.java
@@ -1,4 +1,4 @@
-package com.kh.switchswitch.member;
+package com.kh.switchswitch.card;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -11,13 +11,15 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.kh.switchswitch.member.validator.JoinForm;
+import com.kh.switchswitch.member.model.dto.Member;
 
 
 //가상으로 만들어지는 web.xml을 사용해 테스트환경을 구축
@@ -27,7 +29,7 @@ import com.kh.switchswitch.member.validator.JoinForm;
 @RunWith(SpringJUnit4ClassRunner.class)
 //가상의 applicationContext를 생성할 때 사용할 spring bean 설정파일의 위치를 지정
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*-context.xml"})
-public class MemberControllerTest {
+public class CardControllerTest {
 	
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -41,41 +43,30 @@ public class MemberControllerTest {
 		this.mockMvc = webAppContextSetup(context).build();
 	}
 	
-	//이메일 중복검사 - 성공
 	@Test
-	public void emailCheckWithSuccess() throws Exception {
-		mockMvc.perform(get("/member/email-check")
-				.param("memberEmail","prac@abc.com"))
-			.andExpect(status().isOk())
-			.andExpect(content().string("available"))
-			.andDo(print());
-	}
-	
-	//이메일 중복검사 - 실패
-	@Test
-	public void emailCheckWithFailure() throws Exception {
-		mockMvc.perform(get("/member/email-check")
-				.param("memberEmail","test@email.com"))
-			.andExpect(status().isOk())
-			.andExpect(content().string("disable"))
-			.andDo(print());
-	}
-	
-	//이메일 발송 이후 회원가입 완료처리
-	@Test
-	public void joinImpl() throws Exception {
-		JoinForm form = new JoinForm();
-		form.setMemberName("test");
-		form.setMemberEmail("test@email.com");
-		form.setMemberPass("123qwe!@#");
-		form.setMemberTell("01012341234");
-		form.setMemberNick("testNick");
-		form.setZipNo("0123");
-		form.setAddress("서울시");
+	public void cardInsert() throws Exception {
+		MockMultipartFile file1 = new MockMultipartFile("imgList", "OFN1.txt", null, "OFN01".getBytes());
+		MockMultipartFile file2 = new MockMultipartFile("imgList", "OFN2.txt", null, "OFN02".getBytes());
 		
-		mockMvc.perform(get("/member/join-impl/1234")
-				.sessionAttr("persistToken","1234")
-				.sessionAttr("persistUser", form))
+		
+		Member member = new Member();
+		member.setMemberIdx(1);
+
+		mockMvc.perform(multipart("/card/card-form")
+				.file(file1)
+				.file(file2)
+				.contentType(MediaType.MULTIPART_FORM_DATA)
+				.param("category", "category")
+				.param("name", "name")
+				.param("condition", "3")
+				.param("deliveryCharge", "N")
+				.param("content", "본문")
+				.param("region", "게시글테스트메서드")
+				.param("regionDetail", "본문")
+				.param("method", "본문")
+				.param("isfree", "N")
+				.param("memberIdx", "1")
+				.sessionAttr("authentication", member))
 			.andExpect(status().is3xxRedirection())
 			.andDo(print());
 	}
