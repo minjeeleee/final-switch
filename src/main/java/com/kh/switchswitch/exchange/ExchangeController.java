ceb6d7928a262f67f82427622a3364695c21995c
khj <kim37171@cuk.edu>
2021-11-19 19:07:36 +0900

    [김현진] index/exchangeCard 경로 수정

--- src/main/java/com/kh/switchswitch/exchange/ExchangeController.java
+++ src/main/java/com/kh/switchswitch/exchange/ExchangeController.java
@@ -1,5 +1,14 @@
 package com.kh.switchswitch.exchange;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("market")
 public class ExchangeController {
+	
+	@GetMapping("exchangeCard")
+	public void exchangeCard() {}
 
 }
