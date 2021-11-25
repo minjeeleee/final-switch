7aacafa9e5ce8fc602d6c579d206ced87dd307b6
song <sgh83037@naver.com>
2021-11-26 03:31:30 +0900

    [송국희] top mvc추가

---
+++ src/main/java/com/kh/switchswitch/inquiry/controller/TopController.java
@@ -0,0 +1,18 @@
+package com.kh.switchswitch.inquiry.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("inquiry")
+public class TopController {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+
+}
