10f4050c1f77a00cb7488f9bd50aa8b4b9d65ef2
khj <kim37171@cuk.edu>
2021-11-26 07:54:54 +0900

    [김현진,송국희] 카드 교환 진행중

--- src/main/java/com/kh/switchswitch/card/model/repository/CardRequestListRepository.java
+++ src/main/java/com/kh/switchswitch/card/model/repository/CardRequestListRepository.java
@@ -1,16 +1,12 @@
 package com.kh.switchswitch.card.model.repository;
 
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.kh.switchswitch.card.model.dto.CardRequestList;
 
 @Mapper
 public interface CardRequestListRepository {
-
-	@Insert("insert into card_request_list values(sc_req_idx.nextval"
-			+ " , #{requestedCard}, #{requestCard1}, #{requestCard2}, #{requestCard3}, #{requestCard4}"
-			+ " , #{requestedMemIdx}, #{requestMemIdx}, #{propBalance})")
+	
 	void insertCardRequestList(CardRequestList cardRequestList);
 
 }
