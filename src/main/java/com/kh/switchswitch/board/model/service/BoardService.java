d80d1135359c4ac48cc2e597a8ec0c590a8b811c
song <sgh83037@naver.com>
2021-11-26 23:43:18 +0900

    [송국희]update기능

--- src/main/java/com/kh/switchswitch/board/model/service/BoardService.java
+++ src/main/java/com/kh/switchswitch/board/model/service/BoardService.java
@@ -3,6 +3,7 @@ package com.kh.switchswitch.board.model.service;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.switchswitch.board.model.dto.Board;
@@ -15,14 +16,9 @@ public interface BoardService {
 	
 	//게시글 목록
 	Map<String,Object> selectBoardList(int page);
-	//게시글 수정
-	Map<String, Object> findBoardToModify(String bdIdx);
-	
+
 	//게시글 수정
 	void modifyBoard(Board board, List<MultipartFile> files);
-	
-	Map<String, Object> findBoardByIdx(String bdIdx);
-	
 
 
 
