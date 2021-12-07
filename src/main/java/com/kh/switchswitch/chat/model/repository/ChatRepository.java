6fa8cc75947d4a8a638fc85da0f9c1d21e337cc0
minjeeleee <fly12035@gmail.com>
2021-12-07 17:01:15 +0900

    [이민지] 채팅방 생성오류 수정

--- src/main/java/com/kh/switchswitch/chat/model/repository/ChatRepository.java
+++ src/main/java/com/kh/switchswitch/chat/model/repository/ChatRepository.java
@@ -17,6 +17,7 @@ public interface ChatRepository {
 	@Select("select * from chat_messages where chatting_idx=#{chattingIdx}  order by cm_idx asc")
 	List<ChatMessages> selectChatMessagesList(Integer receiverIdx);
 
+	
 	@Insert("insert into chat_messages values(sc_chat_idx.nextval,#{chattingIdx},#{senderId},#{message},sysdate,1)")
 	void insertChatMessage(ChatMessages chatMessages);
 
@@ -26,8 +27,8 @@ public interface ChatRepository {
 	@Update("update chat_messages set is_read=0 where chatting_idx=#{chattingIdx}")
 	void updateAllChatIsRead(Integer chattingIdx);
 
-	@Insert("insert into chatting values(sc_chatting_idx.nextval,#{requestedMemIdx},#{requestedMemIdx2},sysdate)")
-	Object insertChatting(Integer requestedMemIdx, Integer requestedMemIdx2);
+	@Insert("insert into chatting values(sc_chatting_idx.nextval,#{attendee1},#{attendee2},sysdate)")
+	void insertChatting(Chatting chatting);
 	
 	@Insert("insert into chatting_history values(#{chattingIdx},#{attendee1},#{attendee2},#{createAt})")
 	void insertChattingHistory(Chatting chatting);
@@ -47,8 +48,8 @@ public interface ChatRepository {
 	@Select("select * from chatting where chatting_idx=#{chattingIdx}")
 	Chatting selectChattingByChattingIdx(Integer chattingIdx);
 	
-	@Select("select * from chatting where (attendee1 =#{requestedMemIdx} or attendee2=#{requestedMemIdx}) and (attendee1 =#{requestedMemIdx2} or attendee2=#{requestedMemIdx2})")
-	Chatting selectChattingByAttendeeMemIdxs(Integer requestedMemIdx, Integer requestedMemIdx2);
+	@Select("select * from chatting where (attendee1 =#{requestedMemIdx} or attendee2=#{requestedMemIdx}) and (attendee1 =#{requestMemIdx} or attendee2=#{requestMemIdx})")
+	Chatting selectChattingByAttendeeMemIdxs(@Param("requestedMemIdx")Integer requestedMemIdx,@Param("requestMemIdx") Integer requestMemIdx);
 
 	@Update("update chatting set attendee1 = null where chatting_idx = #{chattingIdx}")
 	void updateChattingAttendee1(Integer chattingIdx);
