cdf07940c3e90cccc0c4a27a2f67faed4abf12f7
khj <kim37171@cuk.edu>
2021-12-09 00:15:48 +0900

    merge

--- src/main/java/com/kh/switchswitch/common/socket/handler/ChattingHandler.java
+++ src/main/java/com/kh/switchswitch/common/socket/handler/ChattingHandler.java
@@ -39,12 +39,63 @@ public class ChattingHandler extends TextWebSocketHandler {
 	}
 	// 클라이언트가 서버로 메세지 전송 처리
 	@Override
-    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        System.out.println("#ChattingController, handleMessage");
-        System.out.println(message);
-        
-        for (WebSocketSession s : sessionList) {
-			s.sendMessage(new TextMessage(message.getPayload()));
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		
+		super.handleTextMessage(session, message);
+		Member loginMember = memberRepository.selectMemberByEmailAndDelN(session.getPrincipal().getName());
+		// JSON --> Map으로 변환
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		//새로운 채팅
+		Map<String, String> mapReceive = objectMapper.readValue(message.getPayload(), Map.class);
+		switch (mapReceive.get("cmd")) {
+		// CLIENT 입장
+		case "CMD_ENTER":
+			// 세션 리스트에 저장
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("memberIdx", loginMember.getMemberIdx());
+			map.put("bang_id", mapReceive.get("bang_id"));
+			map.put("session", session);
+			sessionList.add(map);
+			break;
+			
+		// CLIENT 메세지
+		case "CMD_MSG_SEND":
+			// 같은 채팅방에 메세지 전송
+			Integer senderId = 0;
+			for (int i = 0; i < sessionList.size(); i++) {
+				Map<String, Object> mapSessionList = sessionList.get(i);
+				String bang_id = (String) mapSessionList.get("bang_id");
+				WebSocketSession sess = (WebSocketSession) mapSessionList.get("session");
+
+				if (bang_id.equals(mapReceive.get("bang_id"))) {
+					
+					if(loginMember.getMemberIdx().equals(mapSessionList.get("memberIdx"))) {
+						senderId = loginMember.getMemberIdx();
+						ChatMessages chatMessages = new ChatMessages();
+						chatMessages.setChattingIdx(Integer.parseInt(bang_id));
+						chatMessages.setMessage(mapReceive.get("msg"));
+						chatMessages.setSenderId(loginMember.getMemberIdx());
+						chatRepository.insertChatMessage(chatMessages);
+					}else{
+						chatRepository.updateAllChatIsRead(Integer.parseInt(bang_id));
+					}	
+					Map<String, String> mapToSend = new HashMap<String, String>();
+					int isRead = 1;
+					if(sessionList.size() == 2) isRead = 0;
+					
+					Member member =memberRepository.selectMemberWithMemberIdx(senderId);
+					mapToSend.put("bang_id", bang_id);
+					mapToSend.put("cmd", "CMD_MSG_SEND");
+					mapToSend.put("senderId", member.getMemberNick());
+					mapToSend.put("isRead", Integer.toString(isRead));
+					mapToSend.put("msg",mapReceive.get("msg"));
+					
+					String jsonStr = objectMapper.writeValueAsString(mapToSend);
+					sess.sendMessage(new TextMessage(jsonStr));
+				}
+			}
+			break;
 		}
 	}
 
