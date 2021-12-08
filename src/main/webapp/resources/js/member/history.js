cb1b1ca974d54444be49c176d852f3e036370448
minjeeleee <fly12035@gmail.com>
2021-12-08 20:41:19 +0900

    [이민지] 마이페이지 거래내역 ui 및 js 수정

--- src/main/webapp/resources/js/member/history.js
+++ src/main/webapp/resources/js/member/history.js
@@ -4,7 +4,7 @@ var header = "X-CSRF-TOKEN"
 
 function createExchangeHistory(value) {
     var result = '';
-   	var memberIdx= $('input[type="hidden"]').val();
+   	var memberIdx= $('#memberIdx').val();
 	
     result +=  '<tr><td>'+value.eh.exchangeDate+'</td>'
     if(memberIdx == value.eh.requestedMemIdx){
@@ -18,16 +18,16 @@ function createExchangeHistory(value) {
     result += '<td>'+value.crl.propBalance+'</td>'
     result += '<td>'+value.opponentNickList+'</td>'
     if(value.isRate == 0){
-		result += ' <td><a href="#" class="btn btn-danger btn-circle btn-sm"> <i class="far fa-star"></i> </a> </td></tr>'
+		result += ' <td><i class="far fa-star" style="color:red;"></i></td></tr>'
 	}else{
-		result += ' <td><a href="#" class="btn btn-success btn-circle btn-sm"> <i class="far fa-star"></i> </a> </td></tr>'
+		result += ' <td><i class="far fa-star" style="color:green;"></i></td></tr>'
 	}
     return result
 }
 
 function createFreeHistory(value) {
     var result = '';
-   	var memberIdx= $('input[type="hidden"]').val();
+    var memberIdx= $('#memberIdx').val();
 
     result +=  '<tr><td>'+value.eh.exchangeDate+'</td>'
     if(memberIdx == value.eh.requestedMemIdx){
@@ -53,39 +53,43 @@ $(() =>{
    exchangeHistory()
 })
 
-$("input[name='type']:radio").change(function search() {
-    let category= $("input[name='type']:checked").val();
+$("#exchange").on('click',function search() {
                 
      $('.historyList *').remove();
-	if(category == 'exchange'){
-	   exchangeHistory();
-    }
+	 exchangeHistory();
     
-    if(category == 'freeSharing'){
-	    $.ajax({
-	        type: 'get',
-	        url: "/mypage/free-history",
-	        dataType: "json",
-	        async: false,
-	
-	        success: (data) => {
-	
-	            var result = '';
-	
-	            $.each(data, (index, value) => {
-	                result += createFreeHistory(value)
-	            })
-	
-	            $('.historyList').append(result);
-	        },
-	
-	        error: (err) => {
-	            console.log(err)
-	        }
-	
-	    })
-    }
 })
+
+$("#freeSharing").on('click',function search() {
+                
+     $('.historyList *').remove();
+	
+    $.ajax({
+        type: 'get',
+        url: "/mypage/free-history",
+        dataType: "json",
+        async: false,
+
+        success: (data) => {
+
+            var result = '';
+
+            $.each(data, (index, value) => {
+                result += createFreeHistory(value)
+            })
+
+            $('.historyList').append(result);
+        },
+
+        error: (err) => {
+            console.log(err)
+        }
+
+    })
+
+    
+})
+
 
 function exchangeHistory(){
 	 $.ajax({
@@ -110,7 +114,33 @@ function exchangeHistory(){
 	    })
 }
 
+ document.querySelectorAll(".card-box").forEach(function(e){
+    	switch(e.previousElementSibling.value){
+    	case '1' : e.setAttribute("src","../resources/img/ddongCard.png"); break;
+    	case '2' : e.setAttribute("src","../resources/img/BrownCard.png"); break;
+    	case '3' : e.setAttribute("src","../resources/img/silverCard.png"); break;
+    	case '4' : e.setAttribute("src","../resources/img/goldCard.png"); break;
+    	case '5' : e.setAttribute("src","../resources/img/diaCard.png"); break;
+    	default : console.dir("error!!!");
+    	};
+    })
 
+    document.querySelectorAll("#icon-val").forEach(function(e){
+    	switch(e.previousElementSibling.value){
+    	case '전자기기/생활가전' : e.setAttribute("class","fas fa-bolt"); break;
+    	case '가구/인테리어' : e.setAttribute("class","fas fa-couch"); break;
+    	case '유아동' : e.setAttribute("class","fal fa-baby"); break;
+    	case '생활/가공식품' : e.setAttribute("class","fas fa-utensils"); break;
+    	case '스포츠/레저' : e.setAttribute("class","fas fa-basketball-ball"); break;
+    	case '패션/잡화' : e.setAttribute("class","fas fa-tshirt"); break;
+    	case '게임/만화' : e.setAttribute("class","fas fa-gamepad"); break;
+    	case '뷰티/미용' : e.setAttribute("class","fas fa-air-freshener"); break;
+    	case '반려 동물 용품' : e.setAttribute("class","fas fa-paw"); break;
+    	case '도서/티켓/음반' : e.setAttribute("class","fas fa-compact-disc"); break;
+    	case '기타' : e.setAttribute("class","fas fa-cogs"); break;
+    	default : console.dir("error!!!");
+    	};
+    })
 
 
 
