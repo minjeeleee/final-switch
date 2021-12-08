028d2c309d0ef4b7eee8a4b9356c3de16db8a74f
duddns5@naver.com <duddns5@naver.com>
2021-12-09 00:18:35 +0900

    .

--- src/main/webapp/resources/js/admin/adminModifyForm.js
+++ src/main/webapp/resources/js/admin/adminModifyForm.js
@@ -1,16 +1,10 @@
-/**
- * 
- */
- (()=>{
+(()=>{
 	  let cofirmPw = "";
 	  let cofirmNick = document.querySelector('#memberNick').value;
 	  
 	document.querySelector('#btnNickCheck').addEventListener('click', ()=>{
 		  
-		   const nickName = document.querySelector('#memberNick').value;
-		   const password = document.querySelector('#memberPass').value;
-		   const memberIdx = document.querySelector('#memberIdx').value;
-		   
+		   let nickName = document.querySelector('#memberNick').value;
 		   if(nickName){
 				   fetch("/admin/nick-check?nickName="+nickName)
 				   .then(response =>{
@@ -35,50 +29,13 @@
 				}
 	   });
 	   
-	   
-	   
-	   
-	   
 	   document.querySelector('#frm_modify').addEventListener('submit',e=>{
+		   let nickName = document.querySelector('#memberNick').value;
 		   
-		   
-		   const pwReg = /(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[^a-zA-Zㄱ-힣0-9])(?=.{8,})/;
-		   const nickReg = /.*[0-9a-zA-Z가-힣]{2,}"/;
-		   
-		   if(cofirmNick != nickName && nickName != null){
-				console.dir("닉네임 접근");
+			if(cofirmNick != nickName && nickName != null){
 			   alert('닉네임 중복 검사를 하지 않았습니다');
 			   document.querySelector('#memberNick').focus();
 			   e.preventDefault();
 		   }
-		   console.log("접근준비");
-		   
-		   if(password){
-			   if(!pwReg.test(password)){
-					alert('땡');
-					console.log("접근");
-					e.preventDefault();
-					document.querySelector('#pwCheck').innerHTML = '비밀번호는 숫자, 영문자, 특수문자 조합의 8글자 이상인 문자열 입니다.';
-				}
-		   }
-			fetch("/admin/member-profile-edit-success?memberIdx="+memberIdx)
-				.then(response =>{
-					if(response.ok){
-						return response.text()
-					}else{
-						throw new Error(response.status);
-					}
-				})
-				.then(text =>{
-					if(text == 'success'){
-						alert('회원정보가 수정되었습니다.');
-						location.href("admin/member-profile-edit?memberIdx"+memberIdx);
-					}else{
-						alert('에러');
-					}
-				})
-				.catch(error =>{
-					alert('실패');
-				})
 	   })
   })();
